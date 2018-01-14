438b4a8acc28cc2f0ebe367a14e6a65320950b8a
Stuksus <itsoluution@gmail.com>
2018-01-14 04:27:30 +0300

    Add files via upload

--- try.py
+++ try.py
@@ -1,42 +1,57 @@
 import telebot
+import re
 token = "489178856:AAFWlsgZCc-uh_em5e4v-wsLAoP8vX29V44"
 bot = telebot.TeleBot(token)
-slovar = ['БЛЯДЬ','БЛЯТЬ','ЕБАТЬ','ХУЙ','ЧЛЕН','ПИЗДА','НАХУЙ','ЖОПА','БЛЯ','СУКА','БЛЯЯ','ОТПИЗДИТЬ','ПИЗДИТЬ','ОТПИЗЖУ','ПИЗДИЛИ','ПОПИЗЖУ','СПИЗДИЛИ','НАФИГ','ЗАЕБИСЬ','БЛИН','БЛЕИН']
+slovar = ['БЛИН','БЛЕИН']
+locat = []
 endText = ''
+messageText = []
+result = []
 
-# print(messageId)
-# print(chatId)
-# print(message)
-# print(text)
-# print(len(text.split(' ')))
+#[;,.\s(\w+)]for g in range(0, len(delen[i])):
+   # if delen[i][g] == ',' or '.' or '!' or '?':  # заменить на регулярки for g in range(0,len(delen[i])):
+          #  if delen[i][g] == ',' or '.' or '!' or '?': #заменить на регулярки
+
 @bot.message_handler(content_types=["text"])
 def handel_text(message):
+    flag = False
     messageId = message.message_id
     chatId = message.chat.id
+    firs_name = message.from_user.first_name
+    last_name = message.from_user.last_name
+    username = message.from_user.username
     text = message.text
     text = text.upper()
     delen =text.split(' ')
-    delenWithOut = text.split(',')
+    print(text)
+    print(delen)
+
+    p2 = re.compile(r'(\W+)')
+    result = p2.split(text)
+    print(result)
+  #  delenWithOut = text.split(',')
+    longRes = len(result)
     longSpace = len(text.split(' '))
-    longWithOut =len(text.split(','))
-    if longWithOut > longSpace:
-        for i in range(0, longWithOut):
-            for j in range(0, len(slovar)):
-                if delenWithOut[i] == slovar[j]:
-                    delenWithOut[i]= 'фунтик'
-                    endText = ' '.join(delenWithOut)
-                    bot.send_message(chatId,endText)
-                    bot.delete_message(chatId, messageId)
+   # longWithOut =len(text.split(','))
+    messageText = [firs_name,last_name,'(',username,')','отправил: ']
+    if messageText[1]== None:
+        messageText.pop(1)
 
-                    print('Сообщение удалено')
-    else:
-        for i in range(0, longSpace):
-            for j in range(0, len(slovar)):
-                if delen[i] == slovar[j]:
-                    bot.delete_message(chatId, messageId)
-                    print('Сообщение удалено')
+    for i in range(0, longRes):
 
+        #result= re.findall(r'(\W+)',text)
 
+        for j in range(0, len(slovar)):
+            if result[i] == slovar[j]:
+                result[i] = 'фунтик'
+                flag = True
+                print('Сообщение удалено')
+    if flag == True:
+       messageText.extend(result)
+       endText = ' '.join(messageText)
+       bot.send_message(chatId, endText)
+       bot.delete_message(chatId, messageId)
 
+#посичтить листы в конце
 
 bot.polling(none_stop = True, interval=0)
