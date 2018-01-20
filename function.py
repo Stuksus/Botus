84bfb916883bb3989fe593206020adf4610e41b3
Stuksus <itsoluution@gmail.com>
2018-01-21 00:22:10 +0300

    Add files via upload

--- function.py
+++ function.py
@@ -1,15 +1,90 @@
+import pymysql.cursors
 
 
+# Функция возвращает connection.
+def getConnection(globalS):
+    # Вы можете изменить параметры соединения.
 
-def creatDataBase(name,connection):
-    sql='CREATE TABLE `%s` ( `id` INT(11) NOT NULL AUTO_INCREMENT , `userName` VARCHAR(255) NOT NULL , `value` INT(11) NOT NULL , `colMate` INT(11) NOT NULL , `colGood` INT(11) NOT NULL , `stat` INT(11) NOT NULL , PRIMARY KEY (`id`) USING BTREE) ENGINE = InnoDB;'
+    connection = pymysql.connect(host=globalS[0],
+                                 user=globalS[1],
+                                 password=globalS[2],
+                                 db=globalS[3],
+                                 charset=globalS[4],
+                                 cursorclass=pymysql.cursors.DictCursor)
+    return connection
+
+def creatDB(globalS,arg):
+    connection = getConnection(globalS)
+    sql='CREATE TABLE `'+arg+'` ( `id` INT(11) NOT NULL AUTO_INCREMENT ,`idUser` INT(11) NOT NULL , `userName` VARCHAR(255) NOT NULL , `countWord` INT(11) NOT NULL , `colMate` INT(11) NOT NULL , `colGood` INT(11) NOT NULL , `stat` INT(11) NOT NULL , PRIMARY KEY (`id`) USING BTREE) ENGINE = InnoDB;'
     try:
         cursor = connection.cursor()
-        print(name)
-        # Выполнить sql и передать 1 параметр.
-        cursor.execute(sql,name)
-
+        cursor.execute(sql)
         connection.commit()
     finally:
-        # Закрыть соединение
+        connection.close()
+
+def selectDB(globalS,columnPrephix,column = 'idUser',table = 'users'):
+    connection = getConnection(globalS)
+    sql='SELECT * FROM `'+table+'` WHERE '+column+' =\''+columnPrephix+'\''
+
+    try:
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return result[0]
+    finally:
+        connection.close()
+
+def deleteDB(globalS, columnPrephix, column='idUser', table='users'):
+    connection = getConnection(globalS)
+    sql = 'DELETE FROM `'+table+'` WHERE '+column+' =\''+columnPrephix+'\''
+
+    try:
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        connection.commit()
+        return
+    finally:
+        connection.close()
+
+def insertDB(globalS,arg, table='users'):
+    connection = getConnection(globalS)
+    sql = 'INSERT INTO `'+table+'`(`idUser`,`userName`, `countWord`, `col_mate`, `colGood`, `stat`) VALUES (%s,%s,%s,%s,%s,%s)'
+
+    try:
+        cursor = connection.cursor()
+        cursor.executemany(sql,arg)
+        connection.commit()
+
+    finally:
+        connection.close()
+
+def updateDB(globalS, arg,column = 'idUser',table='users'):
+    connection = getConnection(globalS)
+    sql = 'UPDATE `'+table+'` SET  idUser = %s,userName=%s, countWord = %s, col_mate=%s,colGood=%s,stat=%s WHERE '+column+'='+arg[0][0]+''
+
+    try:
+        cursor = connection.cursor()
+        cursor.executemany(sql,arg)
+        connection.commit()
+
+
+    finally:
+        connection.close()
+
+
+def selectColumnDB(globalS,researchColumn,columnPrephix=1,column = 'idUser',table = 'users'):
+    connection = getConnection(globalS)
+    if columnPrephix == 1:
+        sql = 'SELECT ' + researchColumn + ' FROM `' + table + '` WHERE 1'
+        print(sql)
+    else:
+        sql='SELECT ' +researchColumn+' FROM `'+table+'` WHERE '+column+ ' =\'' +columnPrephix+ '\''
+        print(sql)
+    try:
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return result
+    finally:
         connection.close()
--- try.py
+++ try.py
@@ -1,52 +1,76 @@
 import telebot
 import re
-
+import function
+globalS = ['localhost','rooting','123123','bot','utf8mb4']
 token = ""
 bot = telebot.TeleBot(token)
 slovar = ['БЛИН','БЛЕИН']
 endText = ''
-test_grup = ['-308755034']
+
 
 
 @bot.message_handler(content_types=["text"])
 def handel_text(message):
 
+    badWord = 0
     flag = False
-
+    memberId = False
     messageId = message.message_id
     chatId = message.chat.id
     firs_name = message.from_user.first_name
     last_name = message.from_user.last_name
     username = message.from_user.username
+    userId = message.from_user.id
     text = message.text
     text = text.upper()
-    for i in range(0,len(test_grup)):
-        if username == test_grup[i]:
-            print("Этот пользователь уже есть в записях")
-        else:
-            test_grup.append(username)
-
     p2 = re.compile(r'(\W+)')
     result = p2.split(text)
     print(result)
+    allWord = re.findall(r'\w+',text)
+    print(allWord)
+    countWord = len(allWord)
+    print(countWord)
     longRes = len(result)
     messageText = [firs_name,last_name,'(',username,')','отправил: ']
     if messageText[1]== None:
         messageText.pop(1)
-
     for i in range(0, longRes):
         for j in range(0, len(slovar)):
             if result[i] == slovar[j]:
                 result[i] = 'фунтик'
+                badWord +=1
                 flag = True
                 print('Сообщение удалено')
     if flag == True:
-       messageText.extend(result)
+       result2 = ' '.join(result)
+       result2 =result2.lower()
        endText = ' '.join(messageText)
-       endText = endText.lower()
-       bot.send_message(chatId, endText)
+       endText2 = endText + ' '+ result2
+       bot.send_message(chatId, endText2)
        bot.delete_message(chatId, messageId)
+    idUser = function.selectColumnDB(globalS,'idUser')
+    goodWord = countWord - badWord
+    print(idUser)
+    print(len(idUser))
+    print(countWord)
+    print(badWord)
+    print(userId)
+    print(goodWord)
+    for i in range(0, len(idUser)):
+        print(idUser[i]["idUser"])
+        if userId != idUser[i]["idUser"]:
+            memberId = True
+        else:
+            memberId = False
 
+    if memberId == False:
+        print("Этот пользователь уже есть в записях")
+    else:
+        stat =(badWord/countWord)*100
+        args = [(str(userId), str(username), str(countWord), str(goodWord), str(badWord), str(int(stat)))]
+        print(args)
+        tets = function.insertDB(globalS,args)
+        print(tets)
 
 
 bot.polling(none_stop = True, interval=0)
